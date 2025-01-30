229471b5499df41206b08fc0f094fcb0649cbf03
ashleynguci <hoangkinboy@gmail.com>
2025-01-31 00:24:47 +0700

    update environment application-dev

--- api-gateway/src/main/java/net/javaguides/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/net/javaguides/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package net.javaguides.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +11,4 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
--- service-registry/src/main/java/net/javaguides/service_registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/net/javaguides/service_registry/ServiceRegistryApplication.java
@@ -3,11 +3,13 @@ package net.javaguides.service_registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
