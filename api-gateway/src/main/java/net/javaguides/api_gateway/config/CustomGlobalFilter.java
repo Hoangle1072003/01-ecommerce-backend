d64252f28a2a9db56aed4ad472a16baf8a4ef8ce
21017511vocongtuananh <127751619+21017511vocongtuananh@users.noreply.github.com>
2025-02-18 15:33:53 +0700

    done

--- api-gateway/src/main/java/net/javaguides/api_gateway/config/CustomGlobalFilter.java
+++ api-gateway/src/main/java/net/javaguides/api_gateway/config/CustomGlobalFilter.java
@@ -46,6 +46,7 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
             "/product-service/api/v1/products/.*",
             "/product-service/api/v1/products",
             "/product-service/api/v1/category",
+            "/product-service/api/v1/category/.*",
             "/payment-service/api/v1/payment/vn-pay-callback",
             "/notification-service/api/v1/.*",
             "/identity-service/api/v1/user/.*",
